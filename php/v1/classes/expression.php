62901ba507c3549dbcd92753460d31daeef87170
Kimo Rosenbaum <kimo@netops-121.sfo1.bitgravity.com>
2011-01-03 13:31:20 -0800

    add some meat to this class

--- php/v1/classes/expression.php
+++ php/v1/classes/expression.php
@@ -37,10 +37,68 @@ class NodegroupsApiExpression {
 	/**
 	 * Parse expression
 	 * @param string $expression
+	 * @param bool $use_cache false to recursively parse
 	 * @return array nodes => array(), children => array()
 	 */
-	public function parseExpression($expr) {
+	public function parseExpression($expr = '', $use_cache = true) {
+		global $driver;
+
+		$entities = array();
+		$entities_exclude = array();
+		$nodegroups = array();
+
 		$expr = $this->sanitizeExpression($expr);
+
+		foreach($this->tokenizeExpression($expr) as $entity) {
+			if(empty($entity)) {
+				continue;
+			}
+
+			$negate = false;
+
+			switch(substr($entity, 0, 1)) {
+				case '#':
+					continue 2;
+				case '!':
+					$entity = ltrim($entity, '!');
+					$negate = true;
+					break;
+			}
+
+			$list = array();
+
+			switch(substr($entity, 0, 1)) {
+				case '&':
+					$list = $this->parseFunction($entity, $use_cache);
+					break;
+				case '@':
+					$nodegroups[$entity] = substr($entity, 0, 1);
+
+					if($use_cache) {
+						$r_entity = $driver->getNodesFromNodegroup($entity);
+					} else {
+						$nodegroup = $driver->getNodegroup($entity);
+						$details = $this->parseExpression($details['nodegroup'], $use_cache);
+						$list = array($details['nodes']);
+					}
+					break;
+				default:
+					$list = array($entity);
+			}
+
+			if($negate) {
+				$entities_exclude = array_merge($entities_exclude, $list);
+			} else {
+				$entities = array_merge($entities, $list);
+			}
+		}
+
+		$nodes = array_diff($entities, $entities_exclude);
+
+		return array(
+			'nodegroups' => $nodegroups,
+			'nodes' => $nodes,
+		);
 	}
 
 	/**
@@ -49,15 +107,177 @@ class NodegroupsApiExpression {
 	 * @return string $expression
 	 */
 	protected function sanitizeExpression($expr) {
+		$expr = str_replace("\n", ',', $expr);
+		$expr = str_replace(', ', ',', $expr);
+		$expr = str_replace(',,', ',', $expr);
+		$expr = str_replace('( ', '(', $expr);
+		$expr = str_replace(' )', ')', $expr);
+		$expr = trim($expr, ',');
+		$expr = trim($expr);
+
+		return $expr;
+	}
+
+	/**
+	 * Tokenize expression
+	 * @param string $expression
+	 * @return array tokens
+	*/
+	protected function tokenizeExpression($expr) {
+		$tokens = array();
+
+		$chars = str_split($expr);
+		$length = strlen($expr) - 1;
+
+		$is_function = false;
+		$is_regex = false;
+		$nested_function = 0;
+		$reset = false;
+		$token = '';
+
+		foreach($chars as $pos => $char) {
+			if($char === '/') {
+				if($is_regex) {
+					if($chars[$pos - 1] != '\\') {
+						$is_regex = false;
+					}
+				} else {
+					$is_regex = true;
+				}
+			}
+
+			if($is_regex) {
+				$token .= $char;
+				continue;
+			}
+
+			switch($char) {
+				case '&':
+					$token .= $char;
+
+					if($is_function) {
+						$nested_function++;
+					}
+
+					$is_function = true;
+
+					break;
+				case ')':
+					$token .= $char;
+
+					if($nested_function > 0) {
+						$nested_function--;
+					} else {
+						$reset = true;
+					}
+
+					break;
+				case ',':
+					if($is_function) {
+						$token .= $char;
+					} else {
+						$reset = true;
+					}
+
+					break;
+				default:
+					$token .= $char;
+			}
+
+			if(($reset) || $pos === $length ) {
+				$tokens[] = $token;
+
+				$is_function = false;
+				$is_regex = false;
+				$nested_function = 0;
+				$reset = false;
+				$token = '';
+			}
+		}
+
+		return $tokens;
 	}
 
 	/**
 	 * Validate expression
+	 * Will return true if &regex is used
 	 * @param string $input
 	 * @param array list of errors (if any)
 	 */
 	public function validateExpression($input) {
-		return false;
+		if(strpos($input, '&regex') !== false) {
+		// TODO: figure out how to still validate the rest of the expression
+			return true;
+		}
+
+		$input = sanitizeExpression($input);
+
+		// Test for only valid characters
+		if(!preg_match('/^[-\w\.\(\)\&\@\,\!\#]+$/i', $input)) {
+			return false;
+		}
+
+		// Unbalanced parens and parens without functions (or visa versa)
+		$ampersands = substr_count($input, '&');
+		$open_parens = substr_count($input, '(');
+		$close_parens = substr_count($input, ')');
+
+		if($open_parens !== $close_parens) {
+			return false;
+		}
+
+		if($ampersands !== $open_parens) {
+			return false;
+		}
+
+		// Test for only valid function names
+		if(strpos($input, '&') !== false) {
+			$function_total = preg_match_all('/\&[^\(]*\(/', $input, $functions);
+			if($function_total == 0) {
+				return false;
+			}
+
+			foreach($functions[0] as $function) {
+				switch($function) {
+					case '&diff(':
+					case '&intersect(':
+					case '&regex(':
+					case '&regexp(':
+					case '&union(':
+						break;
+					default:
+						return false;
+				}
+			}
+		}
+
+		// Test for only valid meta character sequences
+		if(preg_match_all('/[^-\w\.][^-\w\.]/', $input, $double_matches)) {
+			foreach($double_matches[0] as $double) {
+				switch($double) {
+					case ',#':
+					case ',!':
+					case ',&':
+					case ',@':
+					case '!&':
+					case '!@':
+					case '#!':
+					case '#&':
+					case '#@':
+					case '(#':
+					case '(!':
+					case '(&':
+					case '(@':
+					case '),':
+					case '))':
+						break;
+					default:
+						return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }
 
